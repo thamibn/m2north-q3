ccce705ceff73500230e519fb9064a15a971daee
Thamsanca Ntuli <thamibn@live.co.za>
2020-05-21 23:08:22 +0200

    added table and read data from json file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $jsonData = Storage::disk('public')->get('coronavirusStats.json');
+
+        return view('home')->with('jsonData', json_decode($jsonData, true));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Corona Stats</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="card-body">
+                        <table class="table table-sm table-hover">
+                            <thead class="thead-light">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Country</th>
+                                <th scope="col">Total Cases</th>
+                                <th scope="col">New Cases</th>
+                                <th scope="col">Total Deaths</th>
+                                <th scope="col">New Deaths</th>
+                                <th scope="col">Total Discovered</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($jsonData as $row)
+                                <tr>
+                                    <th scope="row">{{ $loop->iteration }}</th>
+                                    <td>{{ $row['country'] }}</td>
+                                    <td>{{ $row['total_cases'] }}</td>
+                                    <td>{{ $row['new_cases'] }}</td>
+                                    <td>{{ $row['total_deaths'] }}</td>
+                                    <td>{{ $row['new_deaths'] }}</td>
+                                    <td>{{ $row['total_discovered'] }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
